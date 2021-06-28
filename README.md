5aebfdac6bacf2002c0a4d290e168f1d688e7aaf
arall <gerard.arall@gmail.com>
2021-06-28 19:11:58 +0200

    Add vulns

--- README.md
+++ README.md
@@ -7,3 +7,6 @@ Useful for testing DAST and SAST tools.
 # Security
 
 If you plan to host this, make sure only /web is reachable!
+
+# Sources
+- https://rules.sonarsource.com/
---
+++ python/code/sqli.py
@@ -0,0 +1,12 @@
+from flask import request
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import text
+from database.users import User
+
+@app.route('hello')
+def hello():
+    id = request.args.get("id")
+    stmt = text("SELECT * FROM users where id=%s" % id) # Query is constructed based on user inputs
+    query = SQLAlchemy().session.query(User).from_statement(stmt) # Noncompliant
+    user = query.one()
+    return "Hello %s" % user.username
